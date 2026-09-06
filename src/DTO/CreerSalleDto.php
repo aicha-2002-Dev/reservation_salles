1c7b86ac6d911fd6b1943df610d961e3b3076a36
aicha-Dev-ZOOZ <aissatou2212002@gmail.com>
2026-09-06 02:00:59 +0000

    feat: créer CreerSalleDto

---
+++ src/DTO/CreerSalleDto.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+final class CreerSalleDTO
+{
+    public function __construct(
+        public readonly string $nom,
+        public readonly string $batiment,
+        public readonly int $capacite,
+        public readonly string $type,
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            nom: $data['nom'],
+            batiment: $data['batiment'],
+            capacite: (int) $data['capacite'],
+            type: $data['type'],
+        );
+    }
+}
