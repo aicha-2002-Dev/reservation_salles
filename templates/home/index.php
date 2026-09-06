2b3a0e71bd9c6625555e34375457a3c7a0d6493f
aicha-Dev-ZOOZ <aissatou2212002@gmail.com>
2026-09-06 23:40:21 +0000

    feat: créer HomeController pour la route d'accueil

---
+++ templates/home/index.php
@@ -0,0 +1,3 @@
+<h2>Bienvenue</h2>
+<p>Application de gestion des réservations de salles universitaires.</p>
+<p><a href="/salles">Voir les salles</a> | <a href="/reservations">Voir les réservations</a></p>
