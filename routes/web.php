30f517726ce3a7bd2b09deaa54d0cabf1e1c8a85
aicha-Dev-ZOOZ <aissatou2212002@gmail.com>
2026-09-10 23:14:23 +0000

    fix: correction u fichier de routage:modification d'une route

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,18 @@ use FastRoute\RouteCollector;
 return function (RouteCollector $r): void {
     $r->addRoute('GET', '/', ['App\Controller\HomeController', 'index']);
 
-    $r->addRoute('GET',  '/salles',              [SalleController::class, 'index']);
-    $r->addRoute('GET',  '/salles/create',        [SalleController::class, 'create']);
-    $r->addRoute('POST', '/salles',               [SalleController::class, 'store']);
-    $r->addRoute('GET',  '/salles/{id:\d+}',      [SalleController::class, 'show']);
-    $r->addRoute('GET',  '/salles/{id:\d+}/edit', [SalleController::class, 'edit']);
-    $r->addRoute('POST', '/salles/{id:\d+}/edit', [SalleController::class, 'update']);
+    // Salles
+    $r->addRoute('GET',  '/salles',                    [SalleController::class, 'index']);
+    $r->addRoute('GET',  '/salles/create',             [SalleController::class, 'create']);
+    $r->addRoute('POST', '/salles',                    [SalleController::class, 'store']);
+    $r->addRoute('GET',  '/salles/{id:\d+}',            [SalleController::class, 'show']);
+    $r->addRoute('GET',  '/salles/{id:\d+}/edit',       [SalleController::class, 'edit']);
+    $r->addRoute('POST', '/salles/{id:\d+}',            [SalleController::class, 'update']);
 
-    $r->addRoute('GET',  '/reservations',                [ReservationController::class, 'index']);
-    $r->addRoute('GET',  '/reservations/create',          [ReservationController::class, 'create']);
-    $r->addRoute('POST', '/reservations',                 [ReservationController::class, 'store']);
-    $r->addRoute('GET',  '/reservations/{id:\d+}',        [ReservationController::class, 'show']);
-    $r->addRoute('POST', '/reservations/{id:\d+}/cancel', [ReservationController::class, 'cancel']);
+    // Réservations
+    $r->addRoute('GET',  '/reservations',                         [ReservationController::class, 'index']);
+    $r->addRoute('GET',  '/reservations/create',                  [ReservationController::class, 'create']);
+    $r->addRoute('POST', '/reservations',                         [ReservationController::class, 'store']);
+    $r->addRoute('GET',  '/reservations/{id:\d+}',               [ReservationController::class, 'show']);
+    $r->addRoute('POST', '/reservations/{id:\d+}/cancel',        [ReservationController::class, 'cancel']);
 };
-
