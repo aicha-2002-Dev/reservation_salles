3dcb14e7855f574c1b627abec98f7c09468d6433
aicha-Dev-ZOOZ <aissatou2212002@gmail.com>
2026-09-10 23:24:35 +0000

    feat: correction controlers,validators et services

--- config/container.php
+++ config/container.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use function DI\autowire;
 
+use App\Controller\ReservationController;
+use App\Controller\SalleController;
 use App\Repository\SalleRepository;
 use App\Repository\SalleRepositoryInterface;
 use App\Repository\ReservationRepository;
 use App\Repository\ReservationRepositoryInterface;
+use App\Service\AnnulerReservationService;
+use App\Service\CreerReservationService;
+use App\Service\CreerSalleService;
+use App\Service\ModifierSalleService;
+use App\Validation\ReservationValidator;
+use App\Validation\SalleValidator;
 
 return function (): \DI\Container {
     $builder = new ContainerBuilder();
@@ -16,6 +24,17 @@ return function (): \DI\Container {
     $builder->addDefinitions([
         SalleRepositoryInterface::class => autowire(SalleRepository::class),
         ReservationRepositoryInterface::class => autowire(ReservationRepository::class),
+
+        ReservationController::class => autowire(),
+        SalleController::class => autowire(),
+
+        AnnulerReservationService::class => autowire(),
+        CreerReservationService::class => autowire(),
+        CreerSalleService::class => autowire(),
+        ModifierSalleService::class => autowire(),
+
+        ReservationValidator::class => autowire(),
+        SalleValidator::class => autowire(),
     ]);
 
     return $builder->build();
