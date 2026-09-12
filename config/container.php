a70d17ec8d328d4801b7b868c1c3c00ef8aa3b8c
aicha-Dev-ZOOZ <aissatou2212002@gmail.com>
2026-09-12 09:48:55 +0000

    feat: câbler RendererInterface -> HtmlRenderer dans le conteneur PHP-DI

--- config/container.php
+++ config/container.php
@@ -5,36 +5,27 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use function DI\autowire;
 
-use App\Controller\ReservationController;
-use App\Controller\SalleController;
+use App\View\RendererInterface;
+use App\View\HtmlRenderer;
+use App\View\JsonRenderer;
 use App\Repository\SalleRepository;
 use App\Repository\SalleRepositoryInterface;
 use App\Repository\ReservationRepository;
 use App\Repository\ReservationRepositoryInterface;
-use App\Service\AnnulerReservationService;
-use App\Service\CreerReservationService;
-use App\Service\CreerSalleService;
-use App\Service\ModifierSalleService;
-use App\Validation\ReservationValidator;
-use App\Validation\SalleValidator;
+
 
 return function (): \DI\Container {
     $builder = new ContainerBuilder();
 
     $builder->addDefinitions([
         SalleRepositoryInterface::class => autowire(SalleRepository::class),
-        ReservationRepositoryInterface::class => autowire(ReservationRepository::class),
+        ReservationRepositoryInterface::class => autowire(ReservationRepository::class),   
+        RendererInterface::class => autowire(HtmlRenderer::class)
 
-        ReservationController::class => autowire(),
-        SalleController::class => autowire(),
 
-        AnnulerReservationService::class => autowire(),
-        CreerReservationService::class => autowire(),
-        CreerSalleService::class => autowire(),
-        ModifierSalleService::class => autowire(),
 
-        ReservationValidator::class => autowire(),
-        SalleValidator::class => autowire(),
+
+        
     ]);
 
     return $builder->build();
